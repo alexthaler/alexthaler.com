92d90cb7e02fd170b43faef73628f0891b84169a
Alex Thaler <alexgthaler@gmail.com>
2013-04-20 12:42:26 -0500

    added build and deploy to grunt script

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,14 @@ module.exports = function (grunt) {
 					'css/main.css':'sass/main.sass'	
 				}
 			}
+		}, 
+		rsync: {
+			dist: {
+				src: "dist/.",
+				dest: "/home/aletha3/alexthaler.com",
+				host: "aletha3@muphrid.dreamhost.com",
+				recursive: "true"
+			}
 		}
 	});
 
@@ -15,9 +23,20 @@ module.exports = function (grunt) {
 	grunt.registerTask('build', ['sass'], function() {
 		grunt.log.writeln('starting build process');
 		shell = require('shelljs');
-		shell.exec('rm -r dist/*')
-		shell.exec('cp index.html dist/')
-		shell.exec('cp -r css dist/')
-		shell.exec('cp -r img dist/')
+		shell.exec('rm -r dist/*');
+		shell.exec('touch dist/dont_delete')
+		shell.exec('cp index.html dist/');
+		shell.exec('cp -r css dist/');
+		shell.exec('cp -r img dist/');
+	});
+
+	grunt.registerTask('rsync', ['build'], function() {
+		grunt.log.writeln('deploying to muphrid');
+		shell = require('shelljs');
+		shell.exec('rsync -r dist/* aletha3@muphrid.dreamhost.com:/home/aletha3/alexthaler.com --rsh=ssh')
 	})
+
+	grunt.registerTask('deploy', 'deploy', function() {
+		grunt.task.run('sass', 'build', 'rsync');
+	});
 }
